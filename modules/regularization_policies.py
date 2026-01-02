a295cf309907e4e5fd398030f09d977c645802ca
venkatesh-beep <venkatesh@bluetree.in>
2026-01-02 18:30:04 +0530

    Update regularization_policies.py

--- modules/regularization_policies.py
+++ modules/regularization_policies.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-def accrual_policies_ui():
+def regularization_policies_ui():
     st.header("📊 Regularization Policies")
     st.success("Coming Soon 🚧 Under construction")
