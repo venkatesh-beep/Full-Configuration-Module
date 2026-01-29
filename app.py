53aba7fb85d0181d61c0eb6abad5d8d4c822288c
venkatesh-beep <venkatesh@bluetree.in>
2026-01-29 20:00:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,39 +1,6 @@
 import streamlit as st
 import time
-
 from services.auth import login_ui
-
-# ---- Core Modules ----
-from modules.paycodes import paycodes_ui
-from modules.paycode_events import paycode_events_ui
-from modules.paycode_combinations import paycode_combinations_ui
-from modules.paycode_event_sets import paycode_event_sets_ui
-from modules.shift_templates import shift_templates_ui
-from modules.shift_template_sets import shift_template_sets_ui
-from modules.schedule_patterns import schedule_patterns_ui
-from modules.schedule_pattern_sets import schedule_pattern_sets_ui
-
-try:
-    from modules.employee_lookup_table import employee_lookup_table_ui
-except Exception:
-    employee_lookup_table_ui = None
-
-try:
-    from modules.organization_location_lookup_table import organization_location_lookup_table_ui
-except Exception:
-    organization_location_lookup_table_ui = None
-
-from modules.accruals import accruals_ui
-from modules.accrual_policies import accrual_policies_ui
-from modules.accrual_policy_sets import accrual_policy_sets_ui
-from modules.timeoff_policies import timeoff_policies_ui
-from modules.timeoff_policy_sets import timeoff_policy_sets_ui
-from modules.regularization_policies import regularization_policies_ui
-from modules.regularization_policy_sets import regularization_policy_sets_ui
-from modules.roles import roles_ui
-from modules.overtime_policies import overtime_policies_ui
-from modules.timecard_updation import timecard_updation_ui
-from modules.punch import punch_ui
 
 # ================= PAGE CONFIG =================
 st.set_page_config(
@@ -44,24 +11,17 @@ st.set_page_config(
 
 # ================= GLOBAL STYLE =================
 st.markdown("""
-    <style>
-    .stApp {
-        background-color: #eef4fb;
-    }
-    </style>
+<style>
+.stApp { background-color: #eef4fb; }
+</style>
 """, unsafe_allow_html=True)
 
 # ================= SESSION =================
 if "token" not in st.session_state:
     st.session_state.token = None
 
-if "HOST" not in st.session_state:
-    st.session_state.HOST = "https://saas-beeforce.labour.tech/"
-
 if "token_issued_at" not in st.session_state:
     st.session_state.token_issued_at = None
-
-logged_in_user = st.session_state.get("username", "Logged User")
 
 # ================= LOGIN =================
 if not st.session_state.token:
@@ -79,8 +39,7 @@ if issued_at and (time.time() - issued_at) >= TOKEN_VALIDITY_SECONDS:
 # ================= SIDEBAR =================
 with st.sidebar:
     st.markdown("#### 👤 Logged in")
-    st.write(logged_in_user)
-
+    st.write(st.session_state.get("username", "User"))
     st.markdown("---")
 
     menu = st.radio(
@@ -111,51 +70,47 @@ with st.sidebar:
     )
 
     st.markdown("---")
-
     if st.button("🚪 Logout"):
         st.session_state.clear()
         st.rerun()
 
 # ================= MAIN =================
-if menu == "Paycodes":
-    paycodes_ui()
-elif menu == "Paycode Events":
-    paycode_events_ui()
-elif menu == "Paycode Combinations":
-    paycode_combinations_ui()
-elif menu == "Paycode Event Sets":
-    paycode_event_sets_ui()
-elif menu == "Shift Templates":
-    shift_templates_ui()
-elif menu == "Shift Template Sets":
-    shift_template_sets_ui()
-elif menu == "Schedule Patterns":
-    schedule_patterns_ui()
-elif menu == "Schedule Pattern Sets":
-    schedule_pattern_sets_ui()
-elif menu == "Emp Lookup Table":
-    employee_lookup_table_ui()
-elif menu == "Org Lookup Table":
-    organization_location_lookup_table_ui()
-elif menu == "Accruals":
-    accruals_ui()
-elif menu == "Accrual Policies":
-    accrual_policies_ui()
-elif menu == "Accrual Policy Sets":
-    accrual_policy_sets_ui()
-elif menu == "Timeoff Policies":
-    timeoff_policies_ui()
-elif menu == "Timeoff Policy Sets":
-    timeoff_policy_sets_ui()
-elif menu == "Regularization Policies":
-    regularization_policies_ui()
-elif menu == "Regularization Policy Sets":
-    regularization_policy_sets_ui()
-elif menu == "Roles":
-    roles_ui()
-elif menu == "Overtime Policies":
-    overtime_policies_ui()
-elif menu == "Timecard Updation":
-    timecard_updation_ui()
-elif menu == "Punch Update":
-    punch_ui()
+from modules.paycodes import paycodes_ui
+from modules.paycode_events import paycode_events_ui
+from modules.paycode_combinations import paycode_combinations_ui
+from modules.paycode_event_sets import paycode_event_sets_ui
+from modules.shift_templates import shift_templates_ui
+from modules.shift_template_sets import shift_template_sets_ui
+from modules.schedule_patterns import schedule_patterns_ui
+from modules.schedule_pattern_sets import schedule_pattern_sets_ui
+from modules.accruals import accruals_ui
+from modules.accrual_policies import accrual_policies_ui
+from modules.accrual_policy_sets import accrual_policy_sets_ui
+from modules.timeoff_policies import timeoff_policies_ui
+from modules.timeoff_policy_sets import timeoff_policy_sets_ui
+from modules.regularization_policies import regularization_policies_ui
+from modules.regularization_policy_sets import regularization_policy_sets_ui
+from modules.roles import roles_ui
+from modules.overtime_policies import overtime_policies_ui
+from modules.timecard_updation import timecard_updation_ui
+from modules.punch import punch_ui
+
+if menu == "Paycodes": paycodes_ui()
+elif menu == "Paycode Events": paycode_events_ui()
+elif menu == "Paycode Combinations": paycode_combinations_ui()
+elif menu == "Paycode Event Sets": paycode_event_sets_ui()
+elif menu == "Shift Templates": shift_templates_ui()
+elif menu == "Shift Template Sets": shift_template_sets_ui()
+elif menu == "Schedule Patterns": schedule_patterns_ui()
+elif menu == "Schedule Pattern Sets": schedule_pattern_sets_ui()
+elif menu == "Accruals": accruals_ui()
+elif menu == "Accrual Policies": accrual_policies_ui()
+elif menu == "Accrual Policy Sets": accrual_policy_sets_ui()
+elif menu == "Timeoff Policies": timeoff_policies_ui()
+elif menu == "Timeoff Policy Sets": timeoff_policy_sets_ui()
+elif menu == "Regularization Policies": regularization_policies_ui()
+elif menu == "Regularization Policy Sets": regularization_policy_sets_ui()
+elif menu == "Roles": roles_ui()
+elif menu == "Overtime Policies": overtime_policies_ui()
+elif menu == "Timecard Updation": timecard_updation_ui()
+elif menu == "Punch Update": punch_ui()
